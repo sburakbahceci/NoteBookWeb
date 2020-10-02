67d4be8d7038152a54279e210f81fabcbf2d4e26
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-02 14:53:24 +0300

    Generate notes controller & add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
