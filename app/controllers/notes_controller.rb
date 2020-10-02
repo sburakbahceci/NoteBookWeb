3f442a639f4f6580b051b82b1b54628503d2d626
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-02 15:37:36 +0300

    Edit & Destroy Notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,10 +21,15 @@ class NotesController < ApplicationController
         
     end
     def update
-        
+        if @note.update(note_params)
+            redirect_to @note
+        else
+            render 'edit'
+        end
     end
     def destroy
-        
+        @note.destroy
+        redirect_to notes_path
     end
     private
     def find_note
