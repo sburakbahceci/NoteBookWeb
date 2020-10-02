0d09d66906936643fb5c5400bdd9019f6da62d8c
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-02 15:51:41 +0300

    Addedd

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_124148) do
+ActiveRecord::Schema.define(version: 2020_10_02_124522) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
